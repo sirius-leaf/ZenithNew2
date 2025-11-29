5b8b7a1ea8edd2f4e783e19381fbf33ae27d4ae5
Saji6787 <Saji6787@gmail.com>
2025-11-29 23:02:23 +0700

    Merge branch store ke branch main

--- backend/config/app.php
+++ backend/config/app.php
@@ -18,7 +18,7 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Application Environment
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------
     |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
