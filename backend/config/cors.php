47f864b189133bfbc5451f56f6a338be60cb162f
Saji6787 <Saji6787@gmail.com>
2025-11-30 00:03:28 +0700

    Fix masalah login

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,6 +6,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173', // ← Frontend Vue
         'http://127.0.0.1:5173',
+        'http://localhost:5174',
+        'http://127.0.0.1:5174',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
